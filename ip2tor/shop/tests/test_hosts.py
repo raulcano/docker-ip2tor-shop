465db198f30cf9eb379cdad80b41df231fc2fbff
Raul <raul.cano.argamasilla@gmail.com>
2022-12-12 11:07:11 +0000

    tests retrieval of enabled and alive hosts

--- ip2tor/shop/tests/test_hosts.py
+++ ip2tor/shop/tests/test_hosts.py
@@ -11,16 +11,11 @@ class TestRetrieveHost():
         assert response.status_code == status.HTTP_200_OK
 
     def test_retrieve_only_host_that_is_enabled_and_alive(self, api_client):
-        hostEnabledAlive = baker.make(Host, is_enabled=True, is_alive=True)
-        hostEnabledNotAlive = baker.make(Host, is_enabled=True, is_alive=False)
-        hostNotEnabledAlive = baker.make(Host, is_enabled=False, is_alive=True)
-        hostNotEnabledNotAlive = baker.make(Host, is_enabled=False, is_alive=False)
+        hostEnabledAlive = baker.make(Host, is_enabled=True, is_alive=True, name="hostEA")
+        hostEnabledNotAlive = baker.make(Host, is_enabled=True, is_alive=False, name="hostENA")
+        hostNotEnabledAlive = baker.make(Host, is_enabled=False, is_alive=True, name="hostNEA")
+        hostNotEnabledNotAlive = baker.make(Host, is_enabled=False, is_alive=False, name="hostNENA")
         
         response = api_client.get('/api/v1/public/hosts/')
-        print(response.data)
-
-        assert False
-
-    # Test retrieve a single Host by ID
-    def test_if_host_exists_returns_200(self, api_client):
-        assert True
+        assert len(response.data) == 1
+        assert response.data[0]['name'] == hostEnabledAlive.name
