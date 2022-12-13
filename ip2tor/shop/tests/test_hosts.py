2ae0e649b14af17e24d66277461bdb12bf3702dc
Raul <raul.cano.argamasilla@gmail.com>
2022-12-13 13:57:50 +0000

    a few more tests

--- ip2tor/shop/tests/test_hosts.py
+++ ip2tor/shop/tests/test_hosts.py
@@ -10,7 +10,7 @@ class TestRetrieveHosts ():
         response = api_client.get('/api/v1/public/hosts/')
         assert response.status_code == status.HTTP_200_OK
 
-    def test_retrieve_only_host_that_is_enabled_and_alive(self, api_client):
+    def test_list_only_host_that_is_enabled_and_alive(self, api_client):
         hostEnabledAlive = baker.make(Host, is_enabled=True, is_alive=True, name="hostEA")
         hostEnabledNotAlive = baker.make(Host, is_enabled=True, is_alive=False, name="hostENA")
         hostNotEnabledAlive = baker.make(Host, is_enabled=False, is_alive=True, name="hostNEA")
@@ -19,3 +19,42 @@ class TestRetrieveHosts ():
         response = api_client.get('/api/v1/public/hosts/')
         assert len(response.data) == 1
         assert response.data[0]['name'] == hostEnabledAlive.name
+
+    def test_retrieve_single_host_enabled_and_alive_returns_200(self, api_client):
+        host = baker.make(Host, is_enabled=True, is_alive=True,)
+        response = api_client.get(f'/api/v1/public/hosts/{host.id}/')
+        print(response.data)
+        assert response.status_code == status.HTTP_200_OK
+        # assert response.data['id'] == host.id
+        assert response.data['site'] == host.site.domain
+        # assert response.data['created_at'] == host.created_at
+        # assert response.data['modified_at'] == host.modified_at
+        assert response.data['ip'] == host.ip
+        assert response.data['is_enabled'] == host.is_enabled
+        assert response.data['is_alive'] == host.is_alive
+        assert response.data['name'] == host.name
+        assert response.data['is_testnet'] == host.is_testnet
+        assert response.data['offers_tor_bridges'] == host.offers_tor_bridges
+        assert response.data['tor_bridge_duration'] == host.tor_bridge_duration
+        assert response.data['tor_bridge_price_initial'] == host.tor_bridge_price_initial
+        assert response.data['tor_bridge_price_extension'] == host.tor_bridge_price_extension
+        assert response.data['offers_rssh_tunnels'] == host.offers_rssh_tunnels
+        assert response.data['rssh_tunnel_price'] == host.rssh_tunnel_price
+        assert response.data['terms_of_service'] == host.terms_of_service
+        assert response.data['terms_of_service_url'] == host.terms_of_service_url
+        assert response.data['ci_date'] == host.ci_date
+        assert response.data['ci_message'] == host.ci_message
+        assert response.data['ci_status'] == host.ci_status
+        assert response.data['owner'] == host.owner.id
+
+    def test_retrieve_single_host_not_enabled_or_alive_returns_404(self, api_client):
+        host1 = baker.make(Host, is_enabled=False, is_alive=True,name="hostNEA")
+        host2 = baker.make(Host, is_enabled=True, is_alive=False,name="hostENA")
+        host3 = baker.make(Host, is_enabled=False, is_alive=False, name="hostNENA")
+        
+        response1 = api_client.get(f'/api/v1/public/hosts/{host1.id}/')
+        response2 = api_client.get(f'/api/v1/public/hosts/{host2.id}/')
+        response3 = api_client.get(f'/api/v1/public/hosts/{host3.id}/')
+        assert response1.status_code == status.HTTP_404_NOT_FOUND
+        assert response2.status_code == status.HTTP_404_NOT_FOUND
+        assert response3.status_code == status.HTTP_404_NOT_FOUND
--- ip2tor/shop/tests/test_purchase_orders.py
+++ ip2tor/shop/tests/test_purchase_orders.py
@@ -1,32 +1,37 @@
 from model_bakery import baker
 from rest_framework import status
+# from django.contrib.auth.models import User
 from shop.models import Host, ShopPurchaseOrder
 from charged.lnpurchase.models import PurchaseOrder
 import pytest
 
 @pytest.fixture
 def create_purchase_order(api_client):
-    def do_create_purchase_order():
-        host = baker.make(Host, is_enabled=True, is_alive=True, name="bridge")
-        product = 'tor_bridge'
-        target = 'myonionaddresherewithaport.onion:80'
-        comment = 'This is my Tor bridge in a new host'
-        po = {
-            'host_id': host.id,
-            'product': product,
-            'target': target,
-            'public_key': '',
-            'comment': comment,
-            'tos_accepted': True,
-        }
+    def do_create_purchase_order(po=None):
+        if po == None:
+            host = baker.make(Host, is_enabled=True, is_alive=True, name="bridge")
+            product = 'tor_bridge'
+            target = 'myonionaddresherewithaport.onion:80'
+            comment = 'This is my Tor bridge in a new host'
+            po = {
+                'host_id': host.id,
+                'product': product,
+                'target': target,
+                'public_key': '',
+                'comment': comment,
+                'tos_accepted': True,
+            }
 
         return api_client.post('/api/v1/public/order/', po)
+    
+    print('setting up')
     yield do_create_purchase_order
+    print('tearing down')
 
 @pytest.mark.django_db
 class TestCreatePurchaseOrder():
     def test_create_empty_po_returns_400(self, create_purchase_order):
-        response = create_purchase_order()
+        response = create_purchase_order({})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         
     def test_create_purchase_order_for_TorBridge(self, create_purchase_order):
@@ -37,10 +42,10 @@ class TestCreatePurchaseOrder():
 
 @pytest.mark.django_db
 class TestRetrievePurchaseOrder():
-    def test_retrieve_purchase_order(self, create_purchase_order, api_client):
+    def test_retrieve_purchase_order_includes_item_details_and_ln_invoices(self, create_purchase_order, api_client):
         response_po = create_purchase_order()
-
         response = api_client.get(f'/api/v1/public/pos/{response_po.data["id"]}/')
+        
         # print(response.data)
         assert response.status_code == status.HTTP_200_OK
         assert 'item_details' in response.data
