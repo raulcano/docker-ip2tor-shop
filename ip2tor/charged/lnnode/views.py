68eece27342601823323cb5db63508e46e29e328
Raúl <raul@email>
2022-12-20 11:16:52 +0100

    little update to the test page for triggering check alive in nodes

--- ip2tor/charged/lnnode/views.py
+++ ip2tor/charged/lnnode/views.py
@@ -6,5 +6,7 @@ from django.http import HttpResponse
 
 
 def check_if_node_is_alive(request, pk):
-    node_alive_check.apply_async(priority=0, args=(pk,), countdown=1)
-    return render(request, 'alive.html', {'pk': pk})
+    node = LndGRpcNode.objects.filter(pk=pk).first()
+    # node_alive_check.apply_async(priority=0, args=(pk,), countdown=1)
+    status, info = node.check_alive_status()
+    return render(request, 'alive.html', {'pk': pk, 'status': status, 'info': info})
