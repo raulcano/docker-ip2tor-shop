ad3bb932c79f921e0e4c7d102b5879ef18a9ea7d
Raúl <raul@email>
2022-12-21 10:30:50 +0100

    updates script to load aliases on startup

---
+++ .docker/aliases.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# I tried creating a service to load on startup , but that didn't work for aliases (for other functions this might be useful)
+# see here: https://linuxconfig.org/how-to-run-script-on-startup-on-ubuntu-20-04-focal-fossa-server-desktop
+
+# It turns out that aliases won't be loaded in the system if they run via a script like this:
+# https://stackoverflow.com/questions/24054154/how-do-create-an-alias-in-shell-scripts
+
+# In the end, in order to load these aliases in your system, I added them to ~/.bashrc by loading this file, like this:
+
+# if [ -f /path/to/aliases.sh ]; then
+# . /path/to/aliases.sh
+# fi
+
+# Instructions found here: https://stackoverflow.com/questions/3952033/how-can-i-automatically-load-alias-on-startup#3952039
+
+alias test='docker exec ip2tor-shop-django-http pytest /home/ip2tor/ip2tor/'
+alias stest='sudo docker exec ip2tor-shop-django-http pytest /home/ip2tor/ip2tor/'
+
+alias nginx='docker-compose up -d --no-deps --build nginx'
+alias snginx='sudo docker-compose up -d --no-deps --build nginx'
+
+alias dhttp='docker-compose up -d --no-deps --build django-http'
+alias sdhttp='sudo docker-compose up -d --no-deps --build django-http'
+
+alias regu="docker exec -it ip2tor-shop-django-http /usr/share/restart-gunicorn.sh"
+
+alias onion='docker exec -it ip2tor-shop-tor cat var/lib/tor/ip2tor-shop_hidden_service/hostname'
