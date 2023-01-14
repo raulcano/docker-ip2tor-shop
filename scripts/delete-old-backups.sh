ca836e3facd1d5c4f07967bb914ed5f3c6f10616
Raúl <raul@email>
2023-01-14 23:54:00 +0100

    update delete backups script to delete all type of backup files after a determined time

--- scripts/delete-old-backups.sh
+++ scripts/delete-old-backups.sh
@@ -3,6 +3,5 @@
 source /home/ip2tor/ip2tor/.env
 
 echo 'Deleting old backup files...'
-find /home/ip2tor/backups/*.tgz -type f -mtime +${DELETE_OLD_BACKUPS_AFTER_DAYS}
-
-find /home/ip2tor/backups/*.tgz -type f -mtime +${DELETE_OLD_BACKUPS_AFTER_DAYS} | xargs rm -f
+find /home/ip2tor/backups/* -type f -mtime +${DELETE_OLD_BACKUPS_AFTER_DAYS}
+find /home/ip2tor/backups/* -type f -mtime +${DELETE_OLD_BACKUPS_AFTER_DAYS} | xargs rm -f
